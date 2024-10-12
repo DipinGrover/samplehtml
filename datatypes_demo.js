4d3c57b0b2a2f6dfc1fbae02b977ed898c781697
grover.dipin007 <grover.dipin007@gmail.com>
2024-10-12 18:18:03 +0000

    added datatypes and variable name rules in javascript

---
+++ datatypes_demo.js
@@ -0,0 +1,7 @@
+let product_price = 150000;
+
+let product_discount = 20;
+
+let product_name = "Apple Iphone 11 Pro Max (512 GB,Black)";
+
+let is_avaialble = true;
---
+++ variable_name.js
@@ -0,0 +1,16 @@
+let my_name = "Saurav";
+console.log(my_name);
+// rule 1 -> variable names can have alphabets digits underscores and dollars
+
+let phone1 = 9090909090;
+let phone2 = 9898898989;
+// rule 2 -> variable name must not start with a digit
+
+// rule 3 -> varaiables names are case sensitive
+let personName = "Dipin";
+let PersonName = "Ramesh";
+let personname = "Kailash";
+
+// rule 4 -> we cannot use keywords for naming of variables
+
+// rules 5 -> we cannot use spaces in between variable names
