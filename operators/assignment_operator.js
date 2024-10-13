1d9313b37b1a17ef2718da58abe4f3c0773b7f33
grover.dipin007 <grover.dipin007@gmail.com>
2024-10-13 18:08:31 +0000

    assignment operators added

---
+++ operators/assignment_operator.js
@@ -0,0 +1,27 @@
+let x = 10;
+
+x += 5;
+
+console.log(x); // prints 15
+
+x -= 5;
+
+console.log(x);// prints 10
+
+x*=2;// prints 20
+
+console.log(x);
+
+x/=2;// prints 10
+
+console.log(x);
+
+x%=3;// prints 1
+
+console.log(x);
+
+x**=2;// prints 1
+
+console.log(x);
+
+// these all are assignment operators
