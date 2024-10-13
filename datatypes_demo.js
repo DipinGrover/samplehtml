d684c725b24872156e66ba72bac0cdfdf050907d
grover.dipin007 <grover.dipin007@gmail.com>
2024-10-13 16:44:02 +0000

    added new demo

--- datatypes_demo.js
+++ datatypes_demo.js
@@ -5,3 +5,16 @@ let product_discount = 20;
 let product_name = "Apple Iphone 11 Pro Max (512 GB,Black)";
 
 let is_avaialble = true;
+
+let minus_zero = -0;
+
+console.log(minus_zero);
+
+let x;
+let y = null;
+console.log(x);
+console.log(y);
+
+let very_big_number = 7236136287361281238712630948319034129012019389120n;
+
+console.log(very_big_number);
---
+++ object_demo.js
@@ -0,0 +1,18 @@
+let product = {
+    name: "Apple Iphone 11 Pro Max",
+    price: 150000,
+    discount: 20,
+    is_available: true
+};
+
+console.log(product);
+
+let social_handles = {
+    fb: null,
+    insta: 8000,
+    "linkedin": 2500,
+    x: 1300, 
+    insta: 13000
+};
+
+console.log(social_handles);
