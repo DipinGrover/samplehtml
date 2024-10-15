042b0ff750537a2e73c695ec16a6f806e502e82b
grover.dipin007 <grover.dipin007@gmail.com>
2024-10-15 17:58:29 +0000

    added shortcircuit and comparison operators

---
+++ operators/comparison_operators.js
@@ -0,0 +1,20 @@
+console.log(10<20);
+console.log(10>20);
+console.log(15<=15);
+console.log(8>=19);
+
+// == operator
+console.log(10==10);
+console.log(10=="10");
+
+// === operator
+console.log(10===10);
+console.log("10"===10);
+
+// != operator 
+console.log(10!=10);
+console.log("10"!=10);
+
+// !== operator
+console.log(10!==10);
+console.log("10"!==10);
