90858fd2ed9f92ebe41021f8d59a5713d1e45675
grover.dipin007 <grover.dipin007@gmail.com>
2024-10-15 11:47:34 +0000

    added the comment for logical operators

--- operators/logical_operators.js
+++ operators/logical_operators.js
@@ -1,12 +1,15 @@
+// logical and
 console.log(false && false);
 console.log(false && true);
 console.log(true && false);
 console.log(true && true);
 
+// logical or
 console.log(false || false);
 console.log(false || true);
 console.log(true || false);
 console.log(true || true);
 
+// logical not
 console.log(!false);
 console.log(!true);
