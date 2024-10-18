9d531685fdecd6ac6c91546508f24a6451727d9a
grover.dipin007 <grover.dipin007@gmail.com>
2024-10-18 15:21:23 +0000

    added loops and unary operators

---
+++ operators/bitwise_operators.js
@@ -0,0 +1,9 @@
+let a = 5;
+let b = 6;
+
+console.log(a & b); // bitwise and
+console.log(a | 4); // bitwise or
+console.log(a ^ b); // bitwise xor
+console.log(1 << 3); // left shift 
+console.log(27 >> 2); // right shift
+console.log(15 >> 1); // return floor value
